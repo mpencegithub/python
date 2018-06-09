5174b0aea97364f168b0baba2a4283ada8613497
Michael Pence <michael.allen.pence@gmail.com>
2018-06-09 15:14:54 -0600

    sync android

---
+++ pyds/sumnumfile.py
@@ -0,0 +1,15 @@
+import re
+fhandle=open('regex_sum_97222.txt')
+numbers=list()
+for line in fhandle :
+	line=line.rstrip()
+#	print(line)
+	number=re.findall('[0-9]+', line)
+	if len(number) != 0 :
+#		print(number)
+		for num in number :
+			num=int(num)
+			numbers.append(num)
+print(len(numbers))
+print(sum(numbers))
+	
